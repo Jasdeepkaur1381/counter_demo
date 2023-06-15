2ca0c14f450bc74e4751ce511c93e79123c980b3
Jasdeepkaur1381 <kjasdeep219@gmail.com>
2023-06-16 05:11:18 +0530

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+const App = () => {
 
-function App() {
+  let [num, setnum] = useState(0);
+
+  const Increm = (e) => {
+    console.log(e.target);
+    setnum(num + 1);
+    // setnum = (e) => {
+    //   return Number(e.target.value) + 1;
+    // };
+  }
+  const Decrem = (e) => {
+    console.log(e.target);
+    if (e.target.value === "0") {
+      alert("sorry zero limit reached");
+    }
+    else {
+      setnum(num - 1);
+    }
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='outerDiv'>
+        <div className='innerDiv'>
+          <h1 className='heading'>
+            {num}
+          </h1>
+          <div className='btnDiv'>
+            <button className='btn' value={num} onClick={Increm}>Increm</button>
+            <button className='btn' value={num} onClick={Decrem}>Decrem</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
-
 export default App;
